93b31b9d4b1334381bb0d7b31845c87adf77bde5
namoosir <saqeebnazmus@gmail.com>
2021-09-18 13:09:42 -0400

    Add basic starter code

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const add_user = async (req, res) => {
+    //TODO
+}
+
+const delete_user = async (req, res) => {
+    //TODO
+}
+
+const get_user = async (req, res) => {
+    //TODO
+}
+
+const get_all_users = async (req, res) => {
+    //TODO
+}
+
+const update_user = async (req, res) => {
+    //TODO
+}
+
+module.exports = {
+    add_user,
+    delete_user,
+    get_user,
+    get_all_users,
+    update_user,
+}
