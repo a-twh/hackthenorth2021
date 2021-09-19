f51f9ceb19fa296377fd020c4b2caeb797faec39
namoosir <saqeebnazmus@gmail.com>
2021-09-18 23:07:55 -0400

    Add, test, and debug apis

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/users");
 
-router.get('/users', userController.get_all_users)
-router.get('/users/:id', userController.get_user)
-router.post('/users', userController.add_user)
-router.put('/users/:id', userController.update_user)
-router.delete('/users/:id', userController.delete_user)
-
+router.get('/users', userController.get_all_users);
+router.get('/users/:id', userController.get_user);
+router.post('/users', userController.add_user);
+router.put('/users/:id', userController.update_user);
+router.delete('/users/:id', userController.delete_user);
 
 module.exports = router;
